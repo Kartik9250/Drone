df1eaf14d29af3258b85710dba5cba1857b34f09
Ayangoel17 <goelayan16@gmail.com>
2023-01-17 12:52:54 +0530

    added the gyro

--- drone/drone.ino
+++ drone/drone.ino
@@ -1,4 +1,11 @@
 #include <Servo.h>
+#include "Wire.h"
+#include <MPU6050_light.h>
+
+
+MPU6050 mpu(Wire);
+
+long timer = 0;
 
 #define potentiometerPin A0
 int servoPin1 = 6; // signal pin for the ESC.
@@ -23,7 +30,21 @@ servo3.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to 
 servo4.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC.
 
 delay(5000); // delay to allow the ESC to recognize the stopped signal.
+Wire.begin();
+
+//mpu setup
+byte status = mpu.begin();
+Serial.print(F("MPU6050 status: "));
+Serial.println(status);
+while(status!=0){ } // stop everything if could not connect to MPU6050
+
+Serial.println(F("Calculating offsets, do not move MPU6050"));
+delay(1000);
+mpu.calcOffsets(true,true); // gyro and accelero
+Serial.println("Done!\n");
+
 }
+
 
 void loop() {
 
@@ -35,4 +56,28 @@ servo1.writeMicroseconds(pwmVal); // send "stop" signal to ESC. Also necessary t
 servo2.writeMicroseconds(pwmVal); // send "stop" signal to ESC. Also necessary to arm the ESC.
 servo3.writeMicroseconds(pwmVal); // send "stop" signal to ESC. Also necessary to arm the ESC.
 servo4.writeMicroseconds(pwmVal); // send "stop" signal to ESC. Also necessary to arm the ESC.
+
+//mpu output
+mpu.update();
+
+if(millis() - timer > 1000){ // print data every second
+  Serial.print(F("TEMPERATURE: "));Serial.println(mpu.getTemp());
+  Serial.print(F("ACCELERO  X: "));Serial.print(mpu.getAccX());
+  Serial.print("\tY: ");Serial.print(mpu.getAccY());
+  Serial.print("\tZ: ");Serial.println(mpu.getAccZ());
+
+  Serial.print(F("GYRO      X: "));Serial.print(mpu.getGyroX());
+  Serial.print("\tY: ");Serial.print(mpu.getGyroY());
+  Serial.print("\tZ: ");Serial.println(mpu.getGyroZ());
+
+  Serial.print(F("ACC ANGLE X: "));Serial.print(mpu.getAccAngleX());
+  Serial.print("\tY: ");Serial.println(mpu.getAccAngleY());
+  
+  Serial.print(F("ANGLE     X: "));Serial.print(mpu.getAngleX());
+  Serial.print("\tY: ");Serial.print(mpu.getAngleY());
+  Serial.print("\tZ: ");Serial.println(mpu.getAngleZ());
+  Serial.println(F("=====================================================\n"));
+  timer = millis();
+}
+
 }
