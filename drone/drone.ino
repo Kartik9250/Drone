75bbb0a7eb4cfebd0d7f3be5b2b8b363227c3ce6
Manan Gupta <guptamanan282@gmail.com>
2023-01-19 14:28:02 +0530

    PID controller improved

--- drone/drone.ino
+++ drone/drone.ino
@@ -12,6 +12,7 @@ int servoPin4 = 11;
 
 int potVal = 0;
 char Incoming_value = 0;
+
 // PID constants
 int kp = 1.3;
 int ki = 0.05;
@@ -24,7 +25,7 @@ Servo servo3;
 Servo servo4;
 
 // Defining throttle
-int throttle = 0;
+float throttle = 0;
 
 // Timers
 unsigned long timer = 0;
@@ -58,14 +59,19 @@ void setup() {
   Wire.begin();
 
   // Initialize MPU6050
-  while(!mpu.begin(MPU6050_SCALE_500DPS, MPU6050_RANGE_8G))
-  {
-    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
-    delay(500);
-  }
+  Wire.begin();
+  byte status = mpu.begin();
 
-  // Calibrate gyroscope. The calibration must be at rest.
-  mpu.calibrateGyro();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+
+  while (status != 0) { // stop everything if could not connect to MPU6050
+    
+    Serial.println(F("Calculating offsets, do not move MPU6050"));
+    delay(1000);
+    mpu.calcOffsets(); // gyro and accelero
+    Serial.println("Done!\n");
+  }
 
 }
 
@@ -106,35 +112,28 @@ void ble(){
   print_val();
   drone_power();
   }
-/*void read_mpu() {
+void read_mpu() {
 
-    // Read normalized values
-    Vector norm = mpu.readNormalizeGyro();
-  
-    // Calculate Pitch, Roll and Yaw
-    pitch = pitch + norm.YAxis * timeStep;
-    roll = roll + norm.XAxis * timeStep;
-    yaw = yaw + norm.ZAxis * timeStep;
-  
-    // Output raw
-    Serial.print(" Pitch = ");Serial.print(pitch);
-    Serial.print(" Roll = ");Serial.print(roll);
-    Serial.print(" Yaw = ");Serial.println(yaw);
-
-    // Wait to full timeStep period
-    delay((timeStep*1000) - (millis() - timer));
-   
-   if(pitch != 0 or roll != 0)
-   //pid_controller();
+    mpu.update();
+    if ((millis() - timer) > 1000) { // print data every 1000ms
+      Serial.print("X/Pitch : ");
+      Serial.print(mpu.getAngleX());
+      Serial.print("\tY/Roll : ");
+      Serial.print(mpu.getAngleY());
+      Serial.print("\tZ/Yaw : ");
+      Serial.println(mpu.getAngleZ());
+      timer = millis();
+    }
+   if(pitch_offset - 0.5 <= pitch <= pitch_offset + 0.5 or roll_offet - 0.5 <= roll <= roll_offet + 0.5)
+   pid_controller()
 }
 
 void pid_controller() {
     float yaw_pid      = 0;
     float pitch_pid    = 0;
     float roll_pid     = 0;
-    //int throttle = pulse_length[mode_mapping[THROTTLE]];
 
-    if (pitch >= 0) {
+    if (pitch >= pitch_offset) {
       pid_temp_error = pitch;
       pitch_prop = pitch_temp_error;
       pitch_inte = pitch_inte + pitch_temp_error*ki;
@@ -142,8 +141,9 @@ void pid_controller() {
       throttle = kp*pitch_prop + pitch_inte + kd*pitch_deri;
       pitch_previous_error = pitch_temp_error;
       servo1.writeMicroseconds(throttle);
+      servo4.writeMicroseconds(throttle);
     }
-    else if (pitch <= 0) {
+    else if (pitch <= pitch_offset) {
       pid_temp_error = -pitch;
       pitch_prop = pitch_temp_error;
       pitch_inte = pitch_inte + pitch_temp_error;
@@ -151,8 +151,9 @@ void pid_controller() {
       throttle = kp*pitch_prop + ki*pitch_inte + kd*pitch_deri;
       pitch_previous_error = pitch_temp_error;
       servo2.writeMicroseconds(throttle);
+      servo3.writeMicroseconds(throttle);
     }
-    else if (roll >= 0) {
+    else if (roll >= roll_offet) {
       pid_temp_error = -roll;
       roll_prop = roll_temp_error;
       roll_inte = roll_inte + roll_temp_error;
@@ -160,17 +161,19 @@ void pid_controller() {
       throttle = kp*roll_prop + ki*roll_inte + kd*roll_deri;
       roll_previous_error = roll_temp_error;
       servo3.writeMicroseconds(throttle);
+      servo4.writeMicroseconds(throttle);
     }
-    else if (roll <= 0) {
+    else if (roll <= roll_offet) {
       pid_temp_error = -roll;
       roll_prop = roll_temp_error;
       roll_inte = roll_inte + roll_temp_error;
       roll_deri = (roll_temp_error - roll_previous_error);
       throttle = kp*roll_prop + ki*roll_inte + kd*roll_deri;
       roll_previous_error = roll_temp_error;
-      servo4.writeMicroseconds(throttle);
+      servo1.writeMicroseconds(throttle);
+      servo2.writeMicroseconds(throttle);
     }
-}*/
+}
 
 void drone_power() {
   Serial.println(throttle);
@@ -206,8 +209,23 @@ void loop() {
 // servo4.writeMicroseconds(pwmVal); // send "stop" signal to ESC. Also necessary to arm the ESC.
 
 // taking the values of pitch, yaw and roll form the help of MPU6050 gyro sensor
-//read_mpu()
+read_mpu();
 
 ble(); 
 // potVal = 0
+delay(5000); //adding a dela of 5 seconds befire further operations
+
+// int count = 0;
+// while(count != 15) {
+//   increase_throttle();
+//   count += 1;
+// }
+
+// delay(5000) // adding a delay of 5 seconds before decreasing the value
+
+// while(count != 0) {
+//   decrease_throttle();
+//   count -= 1;
+// }
+
 }
